902ced38f81cbcbaed0271d895e85f9bb4103497
whatsgoodg <rlwjddl1596@naver.com>
2024-10-05 00:04:43 +0900

    회원가입 api 작성 및 테스팅

--- src/test/java/com/mog/authserver/user/UserInfoControllerTest.java
+++ src/test/java/com/mog/authserver/user/UserInfoControllerTest.java
@@ -1,20 +1,16 @@
 package com.mog.authserver.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mog.authserver.jwt.service.JwtService;
 import com.mog.authserver.user.domain.enums.Gender;
 import com.mog.authserver.user.domain.enums.LoginSource;
 import com.mog.authserver.user.domain.enums.Role;
 import com.mog.authserver.user.dto.UserInfoRequestDTO;
-import com.mog.authserver.user.service.UserInfoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,8 +20,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserInfoController.class)
-@MockBean(JpaMetamodelMappingContext.class)
+@SpringBootTest  // 모든 빈을 로드
+@AutoConfigureMockMvc
 class UserInfoControllerTest {
 
     @Autowired
@@ -33,15 +29,6 @@ class UserInfoControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;  // JSON 데이터를 변환할 ObjectMapper
-
-    @MockBean
-    private SecurityFilterChain securityFilterChain;
-
-    @MockBean
-    private UserInfoService userInfoService;
-    @MockBean
-    private JwtService jwtService;
-
 
     @Test
     void signUpTest() throws Exception {
