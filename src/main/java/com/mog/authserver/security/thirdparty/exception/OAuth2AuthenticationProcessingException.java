6c4f6e27a7cf00d40138e76564c3bd4397924745
whatsgoodg <rlwjddl1596@naver.com>
2024-10-06 20:21:32 +0900

    oauth2.0 로그인을 위한 config, exception 생성

---
+++ src/main/java/com/mog/authserver/security/thirdparty/exception/OAuth2AuthenticationProcessingException.java
@@ -0,0 +1,9 @@
+package com.mog.authserver.security.thirdparty.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class OAuth2AuthenticationProcessingException extends AuthenticationException {
+    public OAuth2AuthenticationProcessingException(String msg) {
+        super(msg);
+    }
+}
