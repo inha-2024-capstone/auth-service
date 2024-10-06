b971edf94ce536dc986c106eaf8f668ea34e501b
whatsgoodg <rlwjddl1596@naver.com>
2024-10-06 21:50:15 +0900

    mapper 추가

--- src/main/java/com/mog/authserver/security/mapper/UserInfoMapper.java
+++ src/main/java/com/mog/authserver/security/mapper/UserInfoMapper.java
@@ -1,7 +1,11 @@
 package com.mog.authserver.security.mapper;
 
+import com.mog.authserver.security.thirdparty.user.OAuth2Provider;
+import com.mog.authserver.security.thirdparty.user.OAuth2UserInfo;
 import com.mog.authserver.security.userdetails.AuthenticatedUserInfo;
 import com.mog.authserver.user.domain.UserInfoEntity;
+import com.mog.authserver.user.domain.enums.LoginSource;
+import com.mog.authserver.user.domain.enums.Role;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
@@ -19,5 +23,33 @@ public class UserInfoMapper {
                 userInfoEntity.getNickName(),
                 authorities
         );
+    }
+
+    public static UserInfoEntity toUserInfoEntity(OAuth2UserInfo oAuth2UserInfo){
+        LoginSource loginSource = getLoginSource(oAuth2UserInfo);
+        return new UserInfoEntity(
+                oAuth2UserInfo.getEmail(),
+                oAuth2UserInfo.getName(),
+                "oauth2user1!1!",
+                Role.USER,
+                null,
+                null,
+                null,
+                oAuth2UserInfo.getNickname(),
+                oAuth2UserInfo.getProfileImageUrl(),
+                loginSource
+        );
+    }
+
+    public static LoginSource getLoginSource(OAuth2UserInfo oAuth2UserInfo) {
+        OAuth2Provider oAuth2Provider = oAuth2UserInfo.getProvider();
+        LoginSource loginSource;
+        switch (oAuth2Provider){
+            case GOOGLE -> loginSource = LoginSource.GOOGLE;
+            case NAVER -> loginSource = LoginSource.NAVER;
+            case KAKAO -> loginSource = LoginSource.KAKAO;
+            default -> throw new RuntimeException("provider 가 존재하지 않습니다.");
+        }
+        return loginSource;
     }
 }
