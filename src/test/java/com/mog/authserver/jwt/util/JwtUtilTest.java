cab55c74ccfe5b6554dfbbd889959d023da602f8
whatsgoodg <rlwjddl1596@naver.com>
2024-10-06 21:32:19 +0900

    CookieUtils 단위 테스트

--- src/test/java/com/mog/authserver/jwt/util/JwtUtilTest.java
+++ src/test/java/com/mog/authserver/jwt/util/JwtUtilTest.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class JwtUtilTest {
