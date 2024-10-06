2b05e0750ce301d71eae91bd58b25f072746f8ba
whatsgoodg <rlwjddl1596@naver.com>
2024-10-06 21:38:13 +0900

    OAuth2.0 provider, Oauth2UserInfo, Oauth2UserInfoFactory 추가

---
+++ src/main/java/com/mog/authserver/security/thirdparty/user/OAuth2UserInfoFactory.java
@@ -0,0 +1,22 @@
+package com.mog.authserver.security.thirdparty.user;
+
+import com.mog.mog_authserver.security.thridparty.exception.OAuth2AuthenticationProcessingException;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId,
+                                                   String accessToken,
+                                                   Map<String, Object> attributes) {
+        if (OAuth2Provider.GOOGLE.getRegistrationId().equals(registrationId)) {
+            return new GoogleOAuth2UserInfo(accessToken, attributes);
+        } else if (OAuth2Provider.NAVER.getRegistrationId().equals(registrationId)) {
+            return new NaverOAuth2UserInfo(accessToken, attributes);
+        } else if (OAuth2Provider.KAKAO.getRegistrationId().equals(registrationId)) {
+            return new KakaoOAuth2UserInfo(accessToken, attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException("Login with " + registrationId + " is not supported");
+        }
+    }
+}
