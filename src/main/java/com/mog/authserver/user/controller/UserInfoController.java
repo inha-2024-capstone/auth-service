ab1e54a4fc074522e91b5750380fb4488dd8e14f
whatsgoodg <rlwjddl1596@naver.com>
2024-10-07 13:39:14 +0900

    /user/info API 추가 및 통합 테스팅

--- src/main/java/com/mog/authserver/user/controller/UserInfoController.java
+++ src/main/java/com/mog/authserver/user/controller/UserInfoController.java
@@ -5,13 +5,18 @@ import com.mog.authserver.common.response.BaseResponseBody;
 import com.mog.authserver.common.status.enums.SuccessStatus;
 import com.mog.authserver.jwt.JwtToken;
 import com.mog.authserver.jwt.service.JwtService;
+import com.mog.authserver.security.userdetails.AuthenticatedUserInfo;
+import com.mog.authserver.user.domain.UserInfoEntity;
 import com.mog.authserver.user.dto.UserInfoRequestDTO;
+import com.mog.authserver.user.dto.UserInfoResponseDTO;
+import com.mog.authserver.user.mapper.UserInfoEntityMapper;
 import com.mog.authserver.user.service.UserInfoService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -52,4 +57,11 @@ public class UserInfoController {
                 .body(SuccessStatus.OK.getBaseResponseBody());
     }
 
+    @GetMapping("/info")
+    public ResponseEntity<BaseResponseBody<UserInfoResponseDTO>> getUserInfo(Authentication authentication){
+        AuthenticatedUserInfo authenticatedUserInfo = (AuthenticatedUserInfo) authentication.getPrincipal();
+        UserInfoEntity userInfoEntity = userInfoService.findUserInfoById(authenticatedUserInfo.id());
+        return ResponseEntity.status(SuccessStatus.OK.getHttpStatus())
+                .body(SuccessStatus.OK.getBaseResponseBody(UserInfoEntityMapper.toUserInfoResponseDTO(userInfoEntity)));
+    }
 }
--- src/test/java/com/mog/authserver/user/UserInfoControllerTest.java
+++ src/test/java/com/mog/authserver/user/UserInfoControllerTest.java
@@ -2,16 +2,21 @@ package com.mog.authserver.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mog.authserver.common.constant.Constant;
+import com.mog.authserver.common.status.enums.SuccessStatus;
 import com.mog.authserver.jwt.JwtToken;
 import com.mog.authserver.jwt.service.JwtService;
 import com.mog.authserver.security.config.SecurityConfig;
 import com.mog.authserver.security.firstparty.filter.JwtValidationFilter;
 import com.mog.authserver.security.userdetails.AuthenticatedUserInfo;
+import com.mog.authserver.user.domain.UserInfoEntity;
 import com.mog.authserver.user.domain.enums.Gender;
 import com.mog.authserver.user.domain.enums.LoginSource;
 import com.mog.authserver.user.domain.enums.Role;
 import com.mog.authserver.user.dto.UserInfoRequestDTO;
+import com.mog.authserver.user.dto.UserInfoResponseDTO;
+import com.mog.authserver.user.mapper.UserInfoEntityMapper;
 import com.mog.authserver.user.service.UserInfoService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,10 +31,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
@@ -59,17 +66,12 @@ class UserInfoControllerTest {
 
     private MockMvc mockMvc;
 
-    private Authentication authentication;
     private UserInfoRequestDTO userInfoRequestDTO;
+
+    private UserInfoResponseDTO userInfoResponseDTO;
 
     @BeforeEach
     public void setup() {
-        // 인증 객체 생성
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("USER"));
-        AuthenticatedUserInfo authenticatedUserInfo = new AuthenticatedUserInfo(1L, "kim", authorities);
-        authentication = new UsernamePasswordAuthenticationToken(authenticatedUserInfo, "", authorities);
-
         // RequestDTO 생성
         userInfoRequestDTO = new UserInfoRequestDTO("rlwjddl234@naver.com",
                 "kim",
@@ -108,7 +110,8 @@ class UserInfoControllerTest {
     @Test
     @DisplayName("refresh test")
     void refreshTest() throws Exception {
-        JwtToken jwtToken = jwtService.generateTokenSet(authentication);
+        UserInfoEntity signUp = userInfoService.signUp(userInfoRequestDTO);
+        JwtToken jwtToken = jwtService.generateTokenSet(createAuthentication(signUp));
 
         mockMvc.perform(get("/user/refresh")
                         .header(Constant.HEADER_REFRESH_TOKEN, jwtToken.getRefreshToken()))
@@ -123,9 +126,7 @@ class UserInfoControllerTest {
     @Test
     @DisplayName("sign in test")
     void loginTest() throws Exception {
-        // 자격증명을 db에 저장
         userInfoService.signUp(userInfoRequestDTO);
-
         // 자격증명을 전달
         String authorization = Base64.getEncoder().encodeToString((userInfoRequestDTO.email() + ":" + userInfoRequestDTO.password()).getBytes());
         // access token, refresh token을 받아야함.
@@ -141,7 +142,8 @@ class UserInfoControllerTest {
     @Test
     @DisplayName("jwt validation test")
     void jwtValidationTest() throws Exception {
-        JwtToken jwtToken = jwtService.generateTokenSet(authentication);
+        UserInfoEntity signUp = userInfoService.signUp(userInfoRequestDTO);
+        JwtToken jwtToken = jwtService.generateTokenSet(createAuthentication(signUp));
 
         mockMvc.perform(get("/user/test")
                         .header(Constant.HEADER_AUTHORIZATION, "Bearer " + jwtToken.getAccessToken()))
@@ -149,5 +151,33 @@ class UserInfoControllerTest {
                 .andExpect(jsonPath("$.isSucceeded").value("true"))
                 .andExpect(jsonPath("$.message").value("성공입니다."));
 
+    }
+
+    @Test
+    @DisplayName("get user info test")
+    void getUserTest() throws Exception {
+        UserInfoEntity signUp = userInfoService.signUp(userInfoRequestDTO);
+        Authentication authentication = createAuthentication(signUp);
+        JwtToken jwtToken = jwtService.generateTokenSet(authentication);
+
+        MvcResult mvcResult = mockMvc.perform(get("/user/info")
+                        .header(Constant.HEADER_AUTHORIZATION, "Bearer " + jwtToken.getAccessToken()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isSucceeded").value("true"))
+                .andExpect(jsonPath("$.message").value("성공입니다."))
+                .andReturn();
+
+        String contentAsString = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        userInfoResponseDTO = UserInfoEntityMapper.toUserInfoResponseDTO(signUp);
+        String responseDtoToJson = objectMapper.writeValueAsString(SuccessStatus.OK.getBaseResponseBody(userInfoResponseDTO));
+        Assertions.assertEquals(contentAsString, responseDtoToJson);
+
+    }
+
+    private Authentication createAuthentication(UserInfoEntity userInfoEntity){
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        AuthenticatedUserInfo authenticatedUserInfo = new AuthenticatedUserInfo(userInfoEntity.getId(), userInfoEntity.getNickName(), authorities);
+        return new UsernamePasswordAuthenticationToken(authenticatedUserInfo, "", authorities);
     }
 }
