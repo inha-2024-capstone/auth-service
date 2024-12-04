eb6840b5af0247d4f1ba0466e363b6ed63505cdc
whatsgoodg <rlwjddl1596@naver.com>
2024-12-05 00:32:28 +0900

    refactor: 헤더 이름 변경

--- src/main/java/com/mog/authserver/common/constant/Constant.java
+++ src/main/java/com/mog/authserver/common/constant/Constant.java
@@ -5,5 +5,5 @@ public class Constant {
     public final static String TOKEN_PREFIX = "Bearer ";
     public final static String HEADER_ACCESS_TOKEN = "access-token";
     public final static String HEADER_REFRESH_TOKEN = "refresh-token";
-    public final static String HEADER_USER_ID = "user-id";
+    public final static String HEADER_USER_ID = "User-Id";
 }
