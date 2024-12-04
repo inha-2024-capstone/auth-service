dfef641cefb66348f3936907e4c37819d935e8af
whatsgoodg <rlwjddl1596@naver.com>
2024-12-04 22:08:54 +0900

    fix: 토큰을 위한 헤더 정보 노출

--- src/main/java/com/mog/authserver/security/config/CorsConfig.java
+++ src/main/java/com/mog/authserver/security/config/CorsConfig.java
@@ -1,9 +1,10 @@
 package com.mog.authserver.security.config;
 
-import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,15 +14,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class CorsConfig {
 
 
-    //@Bean //bean으로 등록하면 withDefault로도 자동으로 적용됨
-    //@Primary
+    @Bean
+    @Primary
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowedMethods(Collections.singletonList("*"));
         config.setExposedHeaders(List.of("access-token", "refresh-token", "user-id"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
